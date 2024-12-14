45abe78739fedfad78651085b2f453c7e4025b54
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-13 20:15:06 -0500

    feat: register Patient model in admin with custom display and filters

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'date_of_birth', 'doctor', 'created_at')
+    list_filter = ('doctor',)
+    search_fields = ('first_name', 'last_name', 'doctor__username')
