9144603576d9f4f65417c1935b13a8f6fcaba4ab
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-13 19:07:43 -0500

    chore: Add session settings

--- hearttrack/settings.py
+++ hearttrack/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Session-based authentication settings
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
