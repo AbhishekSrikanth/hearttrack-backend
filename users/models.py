f622aa7cadd60d96ba2f3c4dd5991d32eb2f84f3
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-13 19:29:19 -0500

    feat: Add custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Custom user model with email as the unique identifier
+    """
+    ROLES = (
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+    )
+    role = models.CharField(max_length=10, choices=ROLES, default='doctor')
+
+    def is_admin(self)-> bool:
+        """
+        Check if the user is an admin
+
+        Returns:
+            bool: True if the user is an admin, False otherwise
+        """
+        return self.role == 'admin'
+
+    def is_doctor(self)-> bool:
+        """
+        Check if the user is a doctor
+
+        Returns:
+            bool: True if the user is a doctor, False otherwise
+        """
+        return self.role == 'doctor'
