25c54bb7c7d69546d03222dd5c49fc53b38295ff
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-13 19:30:03 -0500

    feat: Add serializers for CustomUser model

---
+++ users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role']
+        read_only_fields = ['id']
+
+class AdminCreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
