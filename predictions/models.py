6a48157980f9490bdc584e49b2b35b1bb98856af
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-14 11:21:37 -0500

    feat: add ECGPrediction model with fields for patient, ECG image, prediction result, and softmax outputs

--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from patients.models import Patient
 
 # Create your models here.
+class ECGPrediction(models.Model):
+    
+    CLASSES = [
+        ('N', 'Normal'),
+        ('S', 'Supraventricular'),
+        ('V', 'Ventricular'),
+        ('F', 'Fusion'),
+        ('Q', 'Unknown'),
+    ]
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='predictions')
+    ecg_image = models.ImageField(upload_to='ecg_images/')
+    prediction_result = models.CharField(max_length=1, choices=CLASSES)
+    softmax_outputs = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Classification for {self.patient} - {self.prediction_result}"
