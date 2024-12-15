5b9b9d7101e3fc942263a86ff268e47b95341ebc
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-14 22:33:48 -0500

    fix: update API endpoint for user authentication to use 'api/users/'

--- hearttrack/urls.py
+++ hearttrack/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('api/patients/', include('patients.urls')),
     path('api/predictions/', include('predictions.urls')),
 ]
