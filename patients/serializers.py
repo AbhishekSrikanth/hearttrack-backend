798d41b4c1662ff5be1c154e28763fe6a787ba55
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-16 10:35:01 -0500

    feat: add predictions field to PatientSerializer

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
-from .models import Patient
+from patients.models import Patient
+from predictions.serializers import ECGPredictionSerializer
 
 class PatientSerializer(serializers.ModelSerializer):
     doctor = serializers.StringRelatedField(read_only=True)  # Read-only field for the doctor username
+    predictions = ECGPredictionSerializer(many=True, read_only=True, source='predictions')  # Add related predictions
 
     class Meta:
         model = Patient
-        fields = ['id', 'doctor', 'first_name', 'last_name', 'date_of_birth', 'created_at', 'updated_at']
+        fields = ['id', 'doctor', 'first_name', 'last_name', 'date_of_birth', 'created_at', 'updated_at', 'predictions']
         read_only_fields = ['id', 'doctor', 'created_at', 'updated_at']
