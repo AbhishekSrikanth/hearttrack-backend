bc26cbd4b14b389826dbbc5b82d47dabe9bf14cb
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-15 20:33:07 -0500

    temp: Allow all CORS origins

--- hearttrack/settings.py
+++ hearttrack/settings.py
@@ -146,7 +146,4 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^http:\/\/localhost:*([0-9]+)?$",
-    r"^http:\/\/127.0.0.1:*([0-9]+)?$",
-]
+CORS_ALLOW_ALL_ORIGINS = True
