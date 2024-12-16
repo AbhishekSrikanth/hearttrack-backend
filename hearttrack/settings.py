5ed9671035689052c5b67494333198fb37707235
Abhishek Srikanth <abhisrik99@outlook.com>
2024-12-15 19:13:42 -0500

    fix: correct CORS settings to use CORS_ALLOW_CREDENTIALS and update origin regex patterns

--- hearttrack/settings.py
+++ hearttrack/settings.py
@@ -145,8 +145,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-CORS_ALLOWED_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^http://localhost:\d+$",
-    r"^http://127.0.0.1:\d+$",
+    r"^http:\/\/localhost:*([0-9]+)?$",
+    r"^http:\/\/127.0.0.1:*([0-9]+)?$",
 ]
